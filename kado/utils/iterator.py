d4c5c4f5295b58d90a065678e087de396a2d68bd
Jimmy Thrasibule <jimmy@thrasibule.mx>
2018-08-18 13:13:38 +0200

    k/u/iterator: Iterator generator to skip the last item
    Define an iterator generator function which will skip the last element
    of an iterable.
    
    Signed-off-by: Jimmy Thrasibule <jimmy@thrasibule.mx>

---
+++ kado/utils/iterator.py
@@ -0,0 +1,46 @@
+# kado/utils/iterator.py
+# ======================
+#
+# Copying
+# -------
+#
+# Copyright (c) 2018 kado authors.
+#
+# This file is part of the *kado* project.
+#
+# kado is a free software project. You can redistribute it and/or
+# modify if under the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY
+# KIND; including but not limited to the WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with kado.
+# If not, see <http://opensource.org/licenses/MIT>.
+#
+
+
+def xlast(iterable):
+    """Make an iterator that returns all elements from iterable except the
+    last one.
+
+
+    :param iterable: Iterable to get the elements from.
+    :type iterable: ~collections.abc.Iterable
+
+
+    :returns: An iterator where the last element from iterable is missing.
+    :rtype: ~collections.abc.Iterator
+
+    """
+    it = iter(iterable)
+
+    current = next(it)
+    while True:
+        try:
+            follow = next(it)
+        except StopIteration:
+            break
+
+        yield current
+        current = follow
